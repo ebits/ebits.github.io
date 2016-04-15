9498fbb32ca2001aecc1a12daa237e641a96c1e1
Mick Ryan <mick@evilbits.io>
2016-04-14 23:01:35 -0700

    starting over

--- assets/js/no_hero.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  //$('.eb-social-links').addClass('animated fadeInDown');
-  $('.eb-social-links-panel').addClass('fixed animated fadeInDownBig');
-  $('.eb-fixed-logo').toggle()
-});
